5b42ddfdfa4421465964792ea24764c87a19c8ab
John Fitzpatrick <jlfitzpatrick2@gmail.com>
2018-02-18 21:09:11 -0700

    Update setup file and renamed unit test file.

--- setup.py
+++ setup.py
@@ -1,20 +1,34 @@
 """
 Setup tools setup module.
 """
-
 from setuptools import setup, find_packages
 
 setup(
   name="dicompipeline",
   version="0.1.0-SNAPSHOT",
-  description="dicom MRI image training pipeline",
+  description="DICOM MRI image training pipeline",
   author="John Fitzpatrick",
-  packages=find_packages(exclude=["docs", "tests"]),
+  packages=find_packages(exclude=["test"]),
   install_requires=[
+    "attrs",
+    "cycler",
     "docopt",
+    "matplotlib",
     "numpy",
     "Pillow",
+    "pluggy",
+    "pprint",
+    "py",
     "pydicom",
+    "pyparsing",
+    "python-dateutil",
+    "pytz",
+    "scikit-learn",
+    "scipy",
+    "six",
+  ],
+  tests_require=[
+    "pytest",
   ],
   entry_points={
     "console_scripts": [
