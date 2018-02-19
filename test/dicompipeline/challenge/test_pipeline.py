bc4ed2db0b14cde7c15325a701c9678f91c2851b
John Fitzpatrick <jlfitzpatrick2@gmail.com>
2018-02-19 08:11:53 -0700

    Added basic test for running the pipeline.

---
+++ test/dicompipeline/challenge/test_pipeline.py
@@ -0,0 +1,18 @@
+from os import path, listdir
+from pytest import fixture
+from dicompipeline.challenge.pipeline import run_pipeline
+from dicompipeline.challenge.load_dataset import load_dataset
+
+
+@fixture
+def data_dir(request):
+  return path.join(str(request.config.rootdir),
+                   "test",
+                   "fixtures",
+                   "test_dataset")
+
+
+def test_can_run_pipeline(data_dir):
+  images, i_contours = load_dataset(data_dir, None)
+
+  run_pipeline(images, i_contours, None)
